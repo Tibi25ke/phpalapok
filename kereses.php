466178d867fa7deeabbc102b4a15ba2452689bf3
72589401620 <72589401620@szily.hu>
2025-03-04 08:39:25 +0100

    kipróbáltuk

--- kereses.php
+++ kereses.php
@@ -1,4 +1,21 @@
 <?php
-$tomb=array(8 , 2, 5 , 3, 7);
-$Indexmax=count($tomb)-1;
-print($Indexmax);
+$tomb=array(8, 2, 5, 3, 7);
+$IndexMax=count($tomb)-1;
+$Elemszam=count($tomb);
+print($IndexMax);
+// print($Elemszam);
+$KeresettElem=5;
+$i=0;
+while($i<($Elemszam)$$($tomb[$i]!=$KeresettElem))
+{
+    $i++;
+}
+if($i<$Elemszam)
+{
+    print("elem megtalálva)");
+    print("indexe:". $i);
+}
+else
+{
+    print("nem található eleem:" .$KeresettElem);
+}
